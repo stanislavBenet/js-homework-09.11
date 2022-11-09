b31b507ee41671aa1f6274d75b45ecbf80df4707
stanislavBenet <stanislav.benet@gmail.com>
2022-11-09 21:11:27 +0200

    Add sript cheks number odd or even

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+let userInput = prompt("Enter number");
 
+if (chekInputIsNumber(Number(userInput))) {
+  if (isNumberOddOrEven(Number(userInput))) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+function isNumberOddOrEven(value1) {
+  return value1 % 2 === 0;
+}
+
+function chekInputIsNumber(value2) {
+  if (isNaN(value2) || value2 === null || value2 === "") {
+    return false;
+  }
+  return true;
+}
